b368f6fb261cff03d81f18ff5055955ae7d00d60
Zed <dev@zedcn.vom>
2017-04-05 13:56:35 +0800

    Fixed checksum field

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -6,13 +6,14 @@ $url64 = 'https://zpl.io/download-windows-64'
 $chechsumType = 'sha256'
 $checksum32 = 'f75cad82e8fd2715b98fb9bfe4bc97ba72181e656b568d9788dab472361823fd'
 $checksum64 = 'cee098fd4f980c6c2d2ac2df5f46e13288f03d50cd24ea9ae6b4d13cae949655'
-$checksum = if ([Environment]::Is64BitOperatingSystem) {
+$checksum = '';
+if ([Environment]::Is64BitOperatingSystem) {
     Write-Output 'Installing 64 bit version.';
-    $checksum64;
+    $checksum = $checksum64;
 }
 else {
     Write-Output 'Installing 32 bit version.';
-    $checksum32;
+    $checksum = $checksum32;
 }
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum
