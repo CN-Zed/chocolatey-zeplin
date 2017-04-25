2497556be5e0f4952ddcf8c8d2aebda53d8b2460
Zed <dev@zedcn.vom>
2017-04-25 13:31:40 +0800

    update to 0.27.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $silentArgs = "-s"
 $url = 'https://zpl.io/download-windows-32'
 $url64 = 'https://zpl.io/download-windows-64'
 $checksumType = 'sha256';
-$checksum32 = 'ecac8da3abb782e20d4c29ccd6814839c5a5de073700ea30d80486ae37b36707';
-$checksum64 = 'e39ba92256b11d5e35440d526eac0c128d4008bc8f522c2a7c6fe727ddbbbf4a';
+$checksum32 = 'e5bae0f2250b237ef17ab6662cb1b520b65834fc01979cb3aa513fa695f01115';
+$checksum64 = '312ed5a0092fc2e02f6633c169ed100de0ed3bfff6891d3144e528edcab53c42';
 $checksum = '';
 if ([Environment]::Is64BitOperatingSystem) {
     Write-Output 'Installing 64 bit version.';
