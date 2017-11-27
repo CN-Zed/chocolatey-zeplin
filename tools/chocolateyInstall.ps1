ce25ac6007414439bcd30005340a1e3bdcbd14a7
Zed <dev@zedcn.com>
2017-11-27 22:47:20 +0800

    update to 1.0.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = "zeplin"
 $fileType = "exe"
 $silentArgs = "-s"
-$url = 'https://pkg.zeplin.io/windows/1.0.2/ia32/zeplin-installer-ia32.exe'
-$url64 = 'https://pkg.zeplin.io/windows/1.0.2/x64/zeplin-installer-x64.exe'
+$url = 'https://pkg.zeplin.io/windows/1.0.3/ia32/zeplin-installer-ia32.exe'
+$url64 = 'https://pkg.zeplin.io/windows/1.0.3/x64/zeplin-installer-x64.exe'
 $checksumType = 'sha256';
-$checksum32 = '8bb41cc2362e7e023ff532317b857a3af89518b92b1a465cddd3bb348dd6cddc';
-$checksum64 = '993b6c7eb46bdc40d922ba005edc48d2aa32f8ecdf9c7b6d4adeede0a55c26b6';
+$checksum32 = '37f502ebaafe95e4eb1661b2af81e4e217235ae409d59cb47a73ea37c2f16fc0';
+$checksum64 = '986f8729791e2bd920d4e4abdb38cc72f157db8f5c757f451e4aa2564f5a89ce';
 $checksum = '';
 if ([Environment]::Is64BitOperatingSystem -and !$env:chocolateyForceX86 ) {
     Write-Output 'Installing 64 bit version.';
